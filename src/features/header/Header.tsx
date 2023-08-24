882429ecd8b7f7ac442a94906ae2cab4b6c1438b
lav0n <leanid.dev@gmail.com>
2023-08-24 10:47:15 +0200

    feat: add new title

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,39 +1,46 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react'
 import styles from './Header.module.css'
-import {useAppDispatch, useAppSelector} from "../../app/store";
-import {getAirport, getSchedule} from "../../app/appReducer";
-import {Button, TextField} from "@mui/material";
-import {useNavigate} from "react-router-dom";
+import { useAppDispatch, useAppSelector } from '../../app/store'
+import { getAirport, getSchedule } from '../../app/appReducer'
+import { Button, TextField } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
 export const Header = () => {
+  const [input, setInput] = useState('')
+  const dispatch = useAppDispatch()
+  const airport = useAppSelector((state) => state.app.airport)
+  const navigator = useNavigate()
 
-    const [input,setInput]=useState('')
-    const dispatch=useAppDispatch()
-    const airport=useAppSelector(state => state.app.airport)
-    const navigator=useNavigate()
-
-    const onClickHandler=()=>{
-        dispatch(getAirport(input))
-        setInput('')
-       navigator('/')
+  const onClickHandler = () => {
+    dispatch(getAirport(input))
+    setInput('')
+    navigator('/')
+  }
+  useEffect(() => {
+    if (airport?.name) {
+      dispatch(getSchedule())
     }
-    useEffect(()=>{
-        if(airport?.name){
-            dispatch(getSchedule())
-        }
-    },[airport])
+  }, [airport])
 
-    return (
-        <div className={styles.block}>
-            <div className={styles.inputBlock}>
-                <TextField type="text" value={input} size="small" placeholder="airport code"
-                       onChange={(e)=>setInput(e.currentTarget.value)}
-               />
-                <Button onClick={onClickHandler} variant="outlined">Find Airport</Button>
-            </div>
-            <div className={styles.airportName}>
-                {airport?.name}
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className={styles.block}>
+      <div className={styles.mainTitle}>Airport schedule</div>
+      <div className={styles.inputBlock}>
+        <TextField
+          type="text"
+          value={input}
+          size="small"
+          placeholder="airport code"
+          onChange={(e) => setInput(e.currentTarget.value)}
+        />
+        <Button
+          onClick={onClickHandler}
+          variant="outlined"
+        >
+          Find Airport
+        </Button>
+      </div>
+      <div className={styles.airportName}>{airport?.name}</div>
+    </div>
+  )
+}
